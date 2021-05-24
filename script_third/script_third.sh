2e286dea74a3bda79dd52f59ed57da3993a5ea2a
Therou <frombayern@gmail.com>
2021-05-24 18:57:37 -0500

    Add initial version

--- script_third/script_third.sh
+++ script_third/script_third.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+GOVERNOR=$1
+REPO_NAME=$2
+
+if [ -z $1 -a -z $2 ]
+then
+	echo "Usage: `basename $0` torvalds linux"
+
+curl https://api.github.com/repos/$GOVERNOR/$REPO_NAME/pulls?state=all 1> ./pull_requests.json
