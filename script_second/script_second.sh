66a02250b1ac4b898cb847d6b51983e4f16f62c2
Therou <frombayern@gmail.com>
2021-05-28 15:22:24 -0500

    Add the third script and modify the second

--- script_second/script_second.sh
+++ script_second/script_second.sh
@@ -1,78 +1,89 @@
 #!/bin/bash
 
+curl -s https://yandex.ru/news/quotes/graph_2000.json 1> ./quotes.json
+
 # March of 2015
-#FIRST_POINT=$(date +%s%3N -d "March 1, 2015 00:00:00") # 1425196800000 in epoch format
-#SECOND_POINT=$(date +%s%3N -d "March 31, 2015 23:59:59") # 1427785200000 in epoch format
-jq -r '.prices[][]' quotes.json |
+FIRST_POINT_2015=$(date +%s%3N -d "March 1, 2015 00:00:00") # 1425196800000 in epoch format
+SECOND_POINT_2015=$(date +%s%3N -d "March 31, 2015 23:59:59") # 1427785200000 in epoch format
+
+MARCH_2015=$(jq -r '.prices[][]' ./quotes.json |
 awk 'ORS=NR%2?" ":"\n"' | 
-awk '$1 >= 1425196800000 && $1 <= 1427785200000 { print $2 }' |
+# awk '$1 >= 1425196800000 && $1 <= 1427785200000 { print $2 }' |
+awk -v first_point=$FIRST_POINT_2015 -v second_point=$SECOND_POINT_2015 '$1 >= first_point && $1 <= second_point { print $2 }' |
 sort |
 awk 'NR==1 { first = $0} END { print; print first}' |
 awk 'ORS=NR%2?" ":"\n"' |
-awk -v diff=0 '{diff=$1-$2} END { print diff }'
-echo "The difference between the highest and lowest values in March 2015"
+awk -v diff=0 '{diff=$1-$2} END { print diff }')
+echo "The difference between the highest and lowest values in March 2015:" "$MARCH_2015"
 
 
 # March of 2016
-#FIRST_POINT=$(date +%s%3N -d "March 1, 2016 00:00:00") # 1456819200000 in epoch format 
-#SECOND_POINT=$(date +%s%3N -d "March 31, 2016 23:59:59") # 1459493999000 in epoch format
-jq -r '.prices[][]' quotes.json |
-awk 'ORS=NR%2?" ":"\n"' | 
-awk '$1 >= 1456819200000 && $1 <= 1459493999000 { print $2 }' |
+FIRST_POINT_2016=$(date +%s%3N -d "March 1, 2016 00:00:00") # 1456819200000 in epoch format 
+SECOND_POINT_2016=$(date +%s%3N -d "March 31, 2016 23:59:59") # 1459493999000 in epoch format
+
+MARCH_2016=$(jq -r '.prices[][]' ./quotes.json |
+awk 'ORS=NR%2?" ":"\n"' |
+awk -v first_point=$FIRST_POINT_2016 -v second_point=$SECOND_POINT_2016 '$1 >= first_point && $1 <= second_point { print $2 }' |
 sort |
 awk 'NR==1 { first = $0} END { print; print first}' |
 awk 'ORS=NR%2?" ":"\n"' |
-awk -v diff=0 '{diff=$1-$2} END { print diff }'
-echo "The difference between the highest and lowest values in March 2016"
+awk -v diff=0 '{diff=$1-$2} END { print diff }')
+echo "The difference between the highest and lowest values in March 2016:" "$MARCH_2016"
 
 
 # March of 2017
-#FIRST_POINT=$(date +%s%3N -d "March 1, 2017 00:00:00") # 1488355200000 in epoch format 
-#SECOND_POINT=$(date +%s%3N -d "March 31, 2017 23:59:59") # 1491029999000 in epoch format
-jq -r '.prices[][]' quotes.json |
-awk 'ORS=NR%2?" ":"\n"' | 
-awk '$1 >= 1488355200000 && $1 <= 1491029999000 { print $2 }' |
+FIRST_POINT_2017=$(date +%s%3N -d "March 1, 2017 00:00:00") # 1488355200000 in epoch format 
+SECOND_POINT_2017=$(date +%s%3N -d "March 31, 2017 23:59:59") # 1491029999000 in epoch format
+
+MARCH_2017=$(jq -r '.prices[][]' ./quotes.json |
+awk 'ORS=NR%2?" ":"\n"' |
+awk -v first_point=$FIRST_POINT_2017 -v second_point=$SECOND_POINT_2017 '$1 >= first_point && $1 <= second_point { print $2 }' |
 sort |
 awk 'NR==1 { first = $0} END { print; print first}' |
 awk 'ORS=NR%2?" ":"\n"' |
-awk -v diff=0 '{diff=$1-$2} END { print diff }'
-echo "The difference between the highest and lowest values in March 2017"
+awk -v diff=0 '{diff=$1-$2} END { print diff }')
+echo "The difference between the highest and lowest values in March 2017:" "$MARCH_2017"
 
 
 # March of 2018
-#FIRST_POINT=$(date +%s%3N -d "March 1, 2018 00:00:00") # 1519891200000 in epoch format 
-#SECOND_POINT=$(date +%s%3N -d "March 31, 2018 23:59:59") # 1522565999000 in epoch format
-jq -r '.prices[][]' quotes.json |
-awk 'ORS=NR%2?" ":"\n"' | 
-awk '$1 >= 1519891200000 && $1 <= 1522565999000 { print $2 }' |
+FIRST_POINT_2018=$(date +%s%3N -d "March 1, 2018 00:00:00") # 1519891200000 in epoch format 
+SECOND_POINT_2018=$(date +%s%3N -d "March 31, 2018 23:59:59") # 1522565999000 in epoch format
+
+MARCH_2018=$(jq -r '.prices[][]' ./quotes.json |
+awk 'ORS=NR%2?" ":"\n"' |
+awk -v first_point=$FIRST_POINT_2018 -v second_point=$SECOND_POINT_2018 '$1 >= first_point && $1 <= second_point { print $2 }' |
 sort |
 awk 'NR==1 { first = $0} END { print; print first}' |
 awk 'ORS=NR%2?" ":"\n"' |
-awk -v diff=0 '{diff=$1-$2} END { print diff }'
-echo "The difference between the highest and lowest values in March 2018"
+awk -v diff=0 '{diff=$1-$2} END { print diff }')
+echo "The difference between the highest and lowest values in March 2018:" "$MARCH_2018"
 
 
 # March of 2019
-#FIRST_POINT=$(date +%s%3N -d "March 1, 2019 00:00:00") # 1551427200000 in epoch format 
-#SECOND_POINT=$(date +%s%3N -d "March 31, 2019 23:59:59") # 1554101999000 in epoch format
-jq -r '.prices[][]' quotes.json |
-awk 'ORS=NR%2?" ":"\n"' | 
-awk '$1 >= 1551427200000 && $1 <= 1554101999000 { print $2 }' |
+FIRST_POINT_2019=$(date +%s%3N -d "March 1, 2019 00:00:00") # 1551427200000 in epoch format 
+SECOND_POINT_2019=$(date +%s%3N -d "March 31, 2019 23:59:59") # 1554101999000 in epoch format
+
+MARCH_2019=$(jq -r '.prices[][]' ./quotes.json |
+awk 'ORS=NR%2?" ":"\n"' |
+awk -v first_point=$FIRST_POINT_2019 -v second_point=$SECOND_POINT_2019 '$1 >= first_point && $1 <= second_point { print $2 }' |
 sort |
 awk 'NR==1 { first = $0} END { print; print first}' |
 awk 'ORS=NR%2?" ":"\n"' |
-awk -v diff=0 '{diff=$1-$2} END { print diff }'
-echo "The difference between the highest and lowest values in March 2019"
+awk -v diff=0 '{diff=$1-$2} END { print diff }')
+echo "The difference between the highest and lowest values in March 2019:" "$MARCH_2019"
 
 
 # March of 2020
-#FIRST_POINT=$(date +%s%3N -d "March 1, 2020 00:00:00") # 1583049600000 in epoch format 
-#SECOND_POINT=$(date +%s%3N -d "March 31, 2020 23:59:59") # 1585724399000 in epoch format
-jq -r '.prices[][]' quotes.json |
+FIRST_POINT_2020=$(date +%s%3N -d "March 1, 2020 00:00:00") # 1583049600000 in epoch format 
+SECOND_POINT_2020=$(date +%s%3N -d "March 31, 2020 23:59:59") # 1585724399000 in epoch format
+
+MARCH_2020=$(jq -r '.prices[][]' ./quotes.json |
 awk 'ORS=NR%2?" ":"\n"' | 
-awk '$1 >= 1583049600000 && $1 <= 1585724399000 { print $2 }' |
+awk -v first_point=$FIRST_POINT_2020 -v second_point=$SECOND_POINT_2020 '$1 >= first_point && $1 <= second_point { print $2 }' |
 sort |
 awk 'NR==1 { first = $0} END { print; print first}' |
 awk 'ORS=NR%2?" ":"\n"' |
-awk -v diff=0 '{diff=$1-$2} END { print diff }'
-echo "The difference between the highest and lowest values in March 2020"
+awk -v diff=0 '{diff=$1-$2} END { print diff }')
+echo "The difference between the highest and lowest values in March 2020:" "$MARCH_2020"
+
+rm ./quotes.json
--- script_third/script_third.sh
+++ script_third/script_third.sh
@@ -3,8 +3,33 @@
 GOVERNOR=$1
 REPO_NAME=$2
 
-if [ -z $1 -a -z $2 ]
+if [ $# -lt 2 ]
 then
-	echo "Usage: `basename $0` torvalds linux"
+	echo "Usage: `basename $0` repo_maintainer repo_name"
+	exit 1
+fi
 
-curl https://api.github.com/repos/$GOVERNOR/$REPO_NAME/pulls?state=all 1> ./pull_requests.json
+#curl https://api.github.com/repos/$GOVERNOR/$REPO_NAME/pulls?state=all 1> ./pull_requests.json 2> /dev/null
+NUMBER_OPEN_PRS=$(curl https://api.github.com/repos/$GOVERNOR/$REPO_NAME/pulls?state=all 2> /dev/null |
+jq -jr '.[] | "PR_state:", " ",.state, "\n","contributor_name:", (.user|" ",.login), "\n", "label:", (.head | " ",.label), "\n"' |
+awk 'ORS=NR%3?" ":"\n"' |
+grep "open" |
+wc -l)
+echo "Number of open pull requests is "$NUMBER_OPEN_PRS""
+
+if [ -z "$NUMBER_OPEN_PRS" ]
+then
+	echo "There are no open pull requests"
+	exit 1
+fi
+
+CONTRIBUTORS_LABELS_PRS=$(curl https://api.github.com/repos/$GOVERNOR/$REPO_NAME/pulls?state=all 2> /dev/null |
+#jq -jr '.[] | "state:", " ",.state, "\n","name:", (.user|" ",.login), "\n"' |
+jq -jr '.[] | "PR_state:", " ",.state, "\n","contributor_name:", (.user|" ",.login), "\n", "label:", (.head | " ",.label), "\n"' |
+awk 'ORS=NR%3?" ":"\n"' |
+grep "open" |
+sort |
+uniq -c |
+awk '{if($1 > 0) print $5,", " $6, $7, ", number of open PRs:", $1}')
+echo "Contributors with more than 1 open PR:"
+echo "$CONTRIBUTORS_LABELS_PRS"
