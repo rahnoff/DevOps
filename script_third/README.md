498976ebbc5abbf0d1635965b6d523f9d1c97e71
Therou <frombayern@gmail.com>
2021-05-30 17:09:06 -0500

    Update README

--- script_third/README.md
+++ script_third/README.md
@@ -1,3 +1,8 @@
 # GitHub PRs checking script
 
 The script accepts two params **repo_maintainer** and **repo_name**. GitHub API returns JSON with info about pull requests
+
+`jq -jr '.[] | "PR_state:", " ",.state, "\n","contributor_name:", (.user|" ",.login), "\n", "label:", (.head | " ",.label), "\n"'` - prints from JSON file only **state**, **login**, **label** fields of each pull request
+`awk 'ORS=NR%3?" ":"\n"'` - formats one column into three ones
+`grep "open"` - prints only lines with word **open**
+`wc -l` - counts number of lines
--- script_third/script_third.sh
+++ script_third/script_third.sh
@@ -15,12 +15,12 @@ awk 'ORS=NR%3?" ":"\n"' |
 grep "open" |
 wc -l)
 
-echo "Number of open pull requests is "$NUMBER_OPEN_PRS""
-
-if [ -z "$NUMBER_OPEN_PRS" ]
+if [ "$NUMBER_OPEN_PRS" = 0 ]
 then
 	echo "There are no open pull requests"
 	exit 1
+else
+	echo "Number of open pull requests is "$NUMBER_OPEN_PRS""
 fi
 
 CONTRIBUTORS_LABELS_PRS=$(curl https://api.github.com/repos/$GOVERNOR/$REPO_NAME/pulls?state=all 2> /dev/null |
@@ -37,7 +37,7 @@ then
 	echo "There are no contributors with 2 or more open PRs"
 	exit 1
 else
-	echo "Contributors with more than 1 open pull request:"
+	echo "Contributors with more than 1 open pull request with labels:"
 	echo "$CONTRIBUTORS_LABELS_PRS"
 	exit 0
 fi
