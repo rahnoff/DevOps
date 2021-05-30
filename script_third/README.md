1627a947d6753d0969c892f94e5199e592a1ed3c
Therou <frombayern@gmail.com>
2021-05-30 16:25:06 -0500

    Update README and script

---
+++ script_third/README.md
@@ -0,0 +1,3 @@
+# GitHub PRs checking script
+
+The script accepts two params **repo_maintainer** and **repo_name**. GitHub API returns JSON with info about pull requests
--- script_third/script_third.sh
+++ script_third/script_third.sh
@@ -3,18 +3,18 @@
 GOVERNOR=$1
 REPO_NAME=$2
 
-if [ $# -lt 2 ]
+if [ "$#" -lt 2 ]
 then
 	echo "Usage: `basename $0` repo_maintainer repo_name"
 	exit 1
 fi
 
-#curl https://api.github.com/repos/$GOVERNOR/$REPO_NAME/pulls?state=all 1> ./pull_requests.json 2> /dev/null
 NUMBER_OPEN_PRS=$(curl https://api.github.com/repos/$GOVERNOR/$REPO_NAME/pulls?state=all 2> /dev/null |
 jq -jr '.[] | "PR_state:", " ",.state, "\n","contributor_name:", (.user|" ",.login), "\n", "label:", (.head | " ",.label), "\n"' |
 awk 'ORS=NR%3?" ":"\n"' |
 grep "open" |
 wc -l)
+
 echo "Number of open pull requests is "$NUMBER_OPEN_PRS""
 
 if [ -z "$NUMBER_OPEN_PRS" ]
@@ -30,6 +30,14 @@ awk 'ORS=NR%3?" ":"\n"' |
 grep "open" |
 sort |
 uniq -c |
-awk '{if($1 > 0) print $5,", " $6, $7, ", number of open PRs:", $1}')
-echo "Contributors with more than 1 open PR:"
-echo "$CONTRIBUTORS_LABELS_PRS"
+awk '{if($1 > 1) print $5,", " $6, $7, ", number of open PRs:", $1}')
+
+if [ -z "$CONTRIBUTORS_LABELS_PRS"  ]
+then
+	echo "There are no contributors with 2 or more open PRs"
+	exit 1
+else
+	echo "Contributors with more than 1 open pull request:"
+	echo "$CONTRIBUTORS_LABELS_PRS"
+	exit 0
+fi
